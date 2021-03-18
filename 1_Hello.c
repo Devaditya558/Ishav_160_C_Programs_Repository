65a295f1e6b993047e04001533fc05906a773883
ishavverma <2020a1r160@mietjammu.in>
2021-03-18 13:03:30 +0530

    A C Program to print Hello World.

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,6 +1,8 @@
 //A basic C Program to print Hello World.
-#include<stdio.h>
-int main(){
-	printf("Hello Buddy!!");
-	return 0;
-	}
+//Start
+#include<stdio.h> //Including standard input ouput header files from library.
+int main(){ 
+	printf("Hello Buddy!!");//Here we entered the texted to be printed.
+	return 0;//Returning.
+	} 
+//End
